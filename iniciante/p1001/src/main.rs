bb64899af299f464c789877cbf382195f6cf12b3
Vitor Mattos <vitorhugo.mattos@dcc.ufrj.br>
2021-08-06 23:23:47 -0300

    solve problem 1001

---
+++ iniciante/p1001/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut a = String::new();
+    let mut b = String::new();
+
+    io::stdin().read_line(&mut a).unwrap();
+    io::stdin().read_line(&mut b).unwrap();
+
+    let a = a.trim().parse::<i32>().unwrap();
+    let b = b.trim().parse::<i32>().unwrap();
+
+    println!("X = {}", a + b);
+}
